1cae22575ae406df99a44852fc493c234a74be63
Tomasz Statkowski <Tomasz.Statkowski@chrobinson.com>
2020-07-21 12:15:06 +0200

    Add wait method and locators

--- selenium-csharp/HomepageFeature.cs
+++ selenium-csharp/HomepageFeature.cs
@@ -1,6 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
 using System.IO;
 using System.Reflection;
 
@@ -17,6 +20,15 @@ namespace selenium_csharp
             var outputDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             _driver = new ChromeDriver(outputDirectory);
             _driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+
+            var loginButtonLocator = By.ClassName("login-button");
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementIsVisible(loginButtonLocator));
+
+            var userNameField = _driver.FindElement(By.Id("user-name"));
+            var passwordField = _driver.FindElement(By.Id("password"));
+            var loginButton = _driver.FindElement(loginButtonLocator);
+
         }
 
         [TestCleanup]
